d59cea44032371397d2ff6e7f7121fe4627c3f11
xeureka <teklemariameureka@gmail.com>
2025-05-16 03:24:38 -0300

    playing with character

---
+++ Intro-programming/patterPinting/HackerRank-C/playingWithCharacter.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+
+    char c;
+    char s[100];
+    char sen[100];
+    
+    scanf("%c", &c);
+    scanf("%s",&s);
+    scanf("%s",&sen);
+
+    printf("%c\n",c);
+    printf("%s\n",s);
+    printf("%s\n",sen);
+    
+    return 0;
+}
+
